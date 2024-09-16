bb1fe3c14f2543f0dfeeba8f1668717b8da588c5
Soheildejnabadi3 <78063320+Soheildejnabadi3@users.noreply.github.com>
2024-09-16 12:59:57 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SwipeCarousel } from './SwipeCarousel';
+
+function App() {
+  return (
+    <div className="App">
+      <SwipeCarousel />
+    </div>
+  );
+}
+
+export default App;
